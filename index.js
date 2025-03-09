e6790aaad3c9e815a5420ae60791f23b1529dc50
RepoRailwayapp <guanyugpt9@gmail.com>
2025-03-09 21:39:01 +0700

    Update index.js

--- index.js
+++ index.js
@@ -7,20 +7,30 @@ const TOKEN = process.env.DISCORD_TOKEN;
 
 // Array Channel ID dari Server A
 const CHANNEL_IDS = [
-    "1345844097486946388", // ID channel chatting lobby
-    "1346085376745541704", // ID channel VIP chatting
-    "1346085392276787230", // ID channel photo n vidio
-    "1347258754369982596", // ID channel link
-    "1347258781309866076" // ID channel req-song
+    "1346007591570112556", // channel general
+    "1346066694723993700" // channel umum
+    // "1228962492076785785", // Pojok - Ncek
+    // "1258709288994607250", // Pojok - AVS
+    // "1184295348466876437", // Act - Bitcoin
+    // "1153033344804733029", // Act - Alts
+    // "1250988860624736278", // Market - Updates
+    // "1186482371596398593", // Rahasia - Market
+    // "1241256779564711938", // Smart - Money
+    // "1319121612750061608" // Web - Corner
 ];
 
 // Array Webhook URL untuk Server B
 const WEBHOOK_URLS = [
-    "https://discord.com/api/webhooks/1346086946254094409/CBtongj7ObnBzgpXXFB7Sh__a72bl5X5vBvhmDvQQjT4MiSgZP8scIeZYLo3AzbexydE", // Webhook channel chatting lobby
-    "https://discord.com/api/webhooks/1346087577094197330/kkwLClsJbQ-MCdgkSpCh60j5tB86sdVpmgiEZdVINsXdaLr5NtUByi_3oa4KkK8K2JsO", //Webhook channel VIP chatting
-    "https://discord.com/api/webhooks/1347256602691899514/YFlgZSJ6QZ3ACQP1h7dSD6U6Iby12cGi8wHu5hppgFB8kgmp3FhsGTrOtMFaRzmHM1SD", //Webhook channel photo n vidio
-    "https://discord.com/api/webhooks/1347248331604234291/jPajzg_q10_muqlPUai9Lc9gJ3BMpcmC7xAV0lM6JuJIkkg439Y1Jm4ckMP8BCzASaNC", //Webhook channel link
-    "https://discord.com/api/webhooks/1347248506888388648/ulD8hoS_iOBSACU-kae9mTYtEcWBO-xybRtubr0j-QR6UbWdXBQxTwOpqtUeKlX86EKg" //Webhook channel req-song
+    "https://discord.com/api/webhooks/1346008467592314911/ZKGl-6hU2RIdIONtl76icOQtqBNJE9XDEbBsxVSn_VBBcvvcnfUjUxLMQI-Z-Q4UgMOm", // Webhook channel general
+    "https://discord.com/api/webhooks/1346067198913024010/JRolTzCR1hJd-msOKtRPq7YkAtmkwPQV5GS74LSo_TnfxrPAmgFHtw8rFMLwx2YKE_ts" // Webhook channel umum
+    //"https://discord.com/api/webhooks/1345110057318350899/amNWqRvNS0aimy2P5Opo_ZZKOI39QidNuX6Rq-ZMA6Q8O-f3XEY__Ogrrt__7YvQ87Rq", // Pojok - Ncek
+    // "https://discord.com/api/webhooks/1346449650000990239/lZbU42fk8gQYWsM605L2sAZSumMybRsGa3fWKzQaRRg6lQsDp0aXcPYXfGS6vLm0fCQP", // Pojok - AVS
+    // "https://discord.com/api/webhooks/1346449858264825897/anA0PkHEvhcWSXGsg_vu0RqE6HhUFQ6F0tlGTFE08o561ga5Uz37HCqsGxuh6IvD6le0", // Act - Bitcoin
+    // "https://discord.com/api/webhooks/1346450008798527580/zkRTeXsmlehxItNe5ddWxIwU9kIODBSbNcW95rDjSbMWJW8bZpOIZuP491sCXlTE8wJf", // Act - Alts
+    // "https://discord.com/api/webhooks/1346450112431390831/nifdNqfed58NrKuxwMzvP63D_p-6XoiN4CLic_CxGvsMlDboJTNCRN-kGSbs1v-Ayd95", // Market - Updates
+    // "https://discord.com/api/webhooks/1346450198850699314/Sd7D727xWO3A4H5YNtdUpkJfetETt0ANVB0i41bFi2z3N0la2sxy1bsPh9XgFH1N14u1", // Rahasia - Market
+    // "https://discord.com/api/webhooks/1346450285895352371/jVEp3URQGYefaxvkY_d3xelMo4lm0HxviEZTM75lA5fcNz7m5_5-PGSfUKrcGcqKMrFA", // Smart - Money
+    // "https://discord.com/api/webhooks/1346450375057608785/wMaukb_8jcZ_m6GuWkBgEh-9ItrtAkYQsIu2AOHeQf2-d63_YHtiqXuartsiBUWi4epg" // Web - Corner
 ];
 
 // File penyimpanan pesan terakhir
